61b9a508ac3d377c4e852eb63284604bd2f9c983
Matt <matt-paul@live.com>
2020-09-14 19:21:56 -0400

    added a function to JS file

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 console.log('app.js is connected!')
 
 // Variables list
+let choice = document.getElementById('top-left')
+
+// array for choices
+const answers = []
+
+choice.addEventListener('click', function(e) {
+    e.preventDefault()
+    choice.innerHTML = 'X'
+    answers.push(choice)
+    console.log(answers)
+})
