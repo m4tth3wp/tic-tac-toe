4367cfc2ed9bf1bbfcf90e6d167d14bb6fe80662
Matt <matt-paul@live.com>
2020-09-15 15:26:37 -0400

    cleaned up JS & CSS file

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ console.log('app.js is connected!')
 // Variables list
 let choice = document.getElementById('1')
 
-// Player name cached
-let playerOne = document.getElementById('player-one')
-let playerTwo = document.getElementById('player-two')
-
-// Winning Message 
-let messageBox = document.getElementById('messages')
-
 //winning arrays
 let winningArrays = [
     [1, 2, 3],
@@ -22,14 +15,27 @@ let winningArrays = [
     [3, 5, 7],
 ]
 
-
 // array for choices
 const answers = []
+
+// ********cached element references **********
+
+// Player name cached
+let playerOne = document.getElementById('player-one')
+let playerTwo = document.getElementById('player-two')
+
+// Winning Message 
+let messageBox = document.getElementById('messages')
+
+// *******Event listeners***********
+
+// when clicking a box, it pushes choice to array
+choice.addEventListener('click', pushChoice)
+
 
 // ******FUNCTIONS******
 
 //push a choice to an array
-
 const pushChoice = (e) => {
     e.preventDefault()
     e.target.textContent = 'X'
@@ -37,27 +43,17 @@ const pushChoice = (e) => {
     console.log(e)
 }
 
-choice.addEventListener('click', pushChoice)
-
 // Highlights a player's name to indicate it is their turn
 const playerHighlight = (player1, player2) => {
     player1.style.backgroundColor = 'yellow'
     player2.style.backgroundColor = 'white'
 }
 
-playerHighlight(playerOne, playerTwo)
-
 //clears game board
-
 const clearBoard = (choice) => {
     for (let choices = 0; choice.textContent; choices++) {
         choice.innerHTML = ''
     }
-}
-
-clearBoard(choice)
-
-playerOne.addEventListener('click', clearBoard)
 
 // Renders the game board
 function gameRender() {
