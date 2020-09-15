0df41221dd3602c33f2056c0e36e438f4755fff2
Matt <matt-paul@live.com>
2020-09-14 21:25:26 -0400

    added JS functions and pseudocode

--- app.js
+++ app.js
@@ -9,15 +9,35 @@ let playerTwo = document.getElementById('player-two')
 
 // Winning Message 
 let messageBox = document.getElementById('messages')
+
+//winning arrays
+let winningArrays = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+]
+
+
 // array for choices
 const answers = []
 
-choice.addEventListener('click', function(e) {
+// ******FUNCTIONS******
+
+//push a choice to an array
+
+const pushChoice = (e) => {
     e.preventDefault()
     e.target.textContent = 'X'
     answers.push(choice)
     console.log(e)
-})
+}
+
+choice.addEventListener('click', pushChoice)
 
 // Highlights a player's name to indicate it is their turn
 const playerHighlight = (player1, player2) => {
@@ -27,4 +47,20 @@ const playerHighlight = (player1, player2) => {
 
 playerHighlight(playerOne, playerTwo)
 
+//clears game board
 
+const clearBoard = (choice) => {
+    for (let choices = 0; choice.textContent; choices++) {
+        choice.innerHTML = ''
+    }
+}
+
+clearBoard(choice)
+
+playerOne.addEventListener('click', clearBoard)
+
+// Renders the game board
+function gameRender() {
+    clearBoard()
+    clearMessage()
+}
