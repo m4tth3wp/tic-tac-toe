498367f3a313316e007f3483790161431c81b51a
Matt <matt-paul@live.com>
2020-09-17 17:52:23 -0400

    completed the switch turns function

--- app2.js
+++ app2.js
@@ -1,8 +1,12 @@
 /*----- constants -----*/
-let gameFinished = false
+const player1 = 'X'
+const player2 = 'O'
 
 /*----- app's state (variables) -----*/
 const pickedboxes = []
+let playerOneTurn= true
+let playerTwoTurn = false
+let gameFinished = false
 
 /*----- cached element references -----*/
 const cellElements = document.querySelectorAll('.boxes')
@@ -12,17 +16,23 @@ let messageBox = document.getElementById('messages')
 
 /*----- event listeners -----*/
 cellElements.forEach(function(cell) {
-    cell.addEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, {once: true})
 
 })
 
 
 /*----- functions -----*/
 function handleClick(e) {
+    // place choice
+    const cell = e.target
+    let playerTurn = playerOneTurn ? player1 : player2 
+    console.log(playerTurn)
     e.preventDefault()
-    e.target.textContent = 'X'
-    pickedboxes.push(e.target)
-    console.log('clicked')
+    placeChoice(cell, playerTurn)
+    // check for win
+    // check for draw 
+    // switch turns
+    switchTurns(playerTurn)
 }
 
 const playerHighlight = (player1, player2) => {
@@ -30,3 +40,21 @@ const playerHighlight = (player1, player2) => {
     player2.style.backgroundColor = 'white'
 }
 
+let placeChoice = function(cell, playerTurn) {
+     if (playerTurn === player1) {
+         cell.textContent = player1
+         cell.classList.add('X')
+     } else {
+         cell.textContent = player2
+         cell.classList.add('O')
+     }
+}
+
+let switchTurns = function() {
+   if (playerOneTurn) {
+    playerOneTurn = false
+    return
+   } else {
+       playerOneTurn = true
+   }
+}
