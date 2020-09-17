1d5d8b10b392cdd724b96e381ecd39ce6c97fa0d
Matt <matt-paul@live.com>
2020-09-17 13:44:35 -0400

    updated the CSS

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 console.log('app.js is connected!')
 
-// Variables list
-let choice = document.getElementById('1')
-
-//winning arrays
+// ********Variables list***********
 let winningArrays = [
     [1, 2, 3],
     [4, 5, 6],
@@ -20,16 +17,14 @@ const answers = []
 
 // ********cached element references **********
 
-// Player name cached
 let playerOne = document.getElementById('player-one')
 let playerTwo = document.getElementById('player-two')
-
-// Winning Message 
 let messageBox = document.getElementById('messages')
+let choice = document.getElementById('1')
+
 
 // *******Event listeners***********
 
-// when clicking a box, it pushes choice to array
 choice.addEventListener('click', pushChoice)
 
 
@@ -54,9 +49,12 @@ const clearBoard = (choice) => {
     for (let choices = 0; choice.textContent; choices++) {
         choice.innerHTML = ''
     }
+}
 
 // Renders the game board
 function gameRender() {
     clearBoard()
     clearMessage()
 }
+
+gameRender()
